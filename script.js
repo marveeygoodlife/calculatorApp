dffeb97c6a9f23cd200e049ce4f3a170810ecd8e
marveeygoodlife <marveey8027@gmail.com>
2026-01-04 19:09:43 +0100

    feat() Add function to handle different maths operation Add button to clear UL Efficiently managed division by zero Use event listener to run operations.

---
+++ script.js
@@ -0,0 +1,74 @@
+"use strict"
+
+//logic to perform math operation
+const calculateBtn = document.getElementById("calculateBtn");
+console.log(calculateBtn);
+
+calculateBtn.addEventListener("click", () => {
+    console.log("connected")
+    const num1 = Number(document.getElementById("num1").value.trim())
+    const num2 = Number(document.getElementById("num2").value.trim());
+    const operator = document.getElementById("operator").value;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        return displayAnswer("Enter a valid number please.");
+}
+    let answer;
+
+    switch (operator) {
+        case "add":
+            answer = add(num1, num2);
+            break;
+        case "subtract":
+            answer = subtract(num1, num2);
+            break;
+        case "multiply":
+            answer = multiply(num1, num2);
+            break;
+        case "divide":
+            if (num2 === 0) {
+                return displayAnswer("Division by zero is not allowed. Try again.");
+    }
+            answer = divide(num1, num2);
+            break;
+    }
+    displayAnswer(formatNumber(answer))
+})
+// handle decimal number
+function formatNumber(value) {
+    return Number.isInteger(value) ? value : value.toFixed(2);
+}
+// function to display calculation
+function displayAnswer(message) {
+    document.getElementById("display").textContent = message;
+}
+
+// function to reset UI
+
+const ClearCalculator = document.getElementById("clearBtn");
+    
+    ClearCalculator.addEventListener("click", () => {
+     document.getElementById("display").textContent = ""; 
+    document.getElementById("num1").value = ""; 
+    document.getElementById("num2").value = ""; 
+    document.getElementById("operator").value = "add";
+ })
+
+// maths functions
+//addition
+function add(num1, num2) {
+    return num1 + num2;
+}
+//subtract
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+//divide
+function divide(num1, num2) {
+    
+    return num1 / num2;
+}
+//multiply
+function multiply(num1, num2) {
+    return num1 * num2;
+}
